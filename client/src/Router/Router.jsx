42b21fadca5dfbd7d6090ba935921b8c31a9ebec
Hridhyapaul <hpdesign2021@gmail.com>
2023-12-08 23:02:51 +0600

    Create pages and routes

--- client/src/Router/Router.jsx
+++ client/src/Router/Router.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
 import { createBrowserRouter } from "react-router-dom";
+import Home from '../pages/Home';
+import SignIn from '../pages/SignIn';
+import SignUp from '../pages/SignUp';
+import About from '../pages/About';
+import Profile from '../pages/Profile';
 
 const router = createBrowserRouter([
     {
       path: "/",
-      element: <div>Hello world!</div>,
+      element: <Home></Home>,
+    },
+    {
+      path: "/sign-in",
+      element: <SignIn></SignIn>,
+    },
+    {
+      path: "/sign-up",
+      element: <SignUp></SignUp>,
+    },
+    {
+      path: "/about",
+      element: <About></About>,
+    },
+    {
+      path: "/profile",
+      element: <Profile></Profile>,
     },
   ]);
 
